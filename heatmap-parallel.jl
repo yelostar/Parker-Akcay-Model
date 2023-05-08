eae5ec620693b6c220cc9ff59ec63a32d9205f0b
yelostar <121648790+yelostar@users.noreply.github.com>
2023-05-08 18:25:15 +0000

    Update heatmap-parallel.jl
    Added method to export data, changed format of dict

--- heatmap-parallel.jl
+++ heatmap-parallel.jl
@@ -3,12 +3,13 @@ using StatsBase
 using FileIO
 using ArgParse
 using Plots
+using DataFrames
+using CSV
 using Distributed
 backend(:plotly)
 
 addprocs(10) 
 @everywhere include("ONS_Fixed_Links.jl")
-
 
 @everywhere function run_worker(inputs, results)
     include("ONS_Fixed_Links.jl")
@@ -17,22 +18,25 @@ addprocs(10)
         println(pard["pn"], " ", pard["pr"], "in pard")
         coopFreq = runSimsReturn(; B=2.0, C=0.5, D=0.0, CL=0.0, gen=500, pnc=pard["pn"], pnd=pard["pn"], pr=pard["pr"], muP=0.001, reps=100)
         println(pard["pn"], " ", pard["pr"], " CF: ", coopFreq[8])
-        pard["data"] = coopFreq
-        put!(results, pard)
+        temp = Dict(zip(keys(pars), coopFreq))
+        put!(results, temp)
     end
 end
 
-function fill_inputs(range,pars)
+function fill_inputs(range,pars, nruns)
     for(i) in 1:range #PNC/PND 
         for(j) in 1:range #PNR 
             vals = (i/(range), j/(20*range))
-            pars["pn"] = vals[1]
-            pars["pr"] = vals[2]
-            println(pars["pn"], " ", pars["pr"], " into inputs")
-            put!(inputs, pars)
+            temp = copy(pars)
+            temp["pn"] = vals[1]
+            temp["pr"] = vals[2]
+            println(temp["pn"], " ", temp["pr"], "into inputs")
+            nruns+=1
+            put!(inputs, temp)
             #vals_arr.push(vals)
         end
     end
+    return nruns
 end
 
 function hmap(results, index::Int, chartTitle::String) #index 8 is coop frequency
@@ -55,28 +59,37 @@ vals_arr = Array{Tuple}
 pars = Dict([
         "pn" => 0.0,
         "pr" => 0.0,
-        "data" => zeros(8)
+        "pnc_end" => 0.0,
+        "pnd_end" => 0.0,
+        "pr_end" => 0.0,
+        "degree" => 0.0,
+        "assortment" => 0.0,
+        "distance" => 0.0,
+        "inclusion" => 0.0,
+        "coopFreq" => 0.0,
     ])
-        
-fill_inputs(10,pars)
+nruns = fill_inputs(10,pars, 0)
 
 for w in workers() # start tasks on the workers to process requests in parallel
     remote_do(run_worker, w, inputs, results)
 end
 
-#hmap(results, 8, "Cooperation Frequencies")
+file = "datacollect_10.csv"
+    cols = push!(sort(collect(keys(pars))),
+                 ["pn", "pr"]...)
+    dat = DataFrame(Dict([(c, Any[]) for c in cols]))
+
+for sim in 1:nruns
+    # get results from parallel jobs
+    flush(stdout)
+    resd = take!(results)
+    # add to table (must convert dict keys to symbols) and save
+    push!(dat, Dict([(Symbol(k), resd[k]) for k in keys(resd)]))
+    CSV.write(file, dat)
+end#hmap(results, 8, "Cooperation Frequencies")
 
 #save("parker-hmap1.jld", "matr", data)
 #currentDict = load("akcay-hmap1.jld")
 #data2 = currentDict["matr"]
 #diff = data[:, :, 8] - data2[:, :, 1]
 #hmap(diff, 10, 1)
-
-#dataArray[1] += network.meanProbNeighborCoop
-#dataArray[2] += network.meanProbNeighborDef
-#dataArray[3] += network.meanProbRandom
-#dataArray[4] += network.meanDegree
-#dataArray[5] += network.meanAssortment
-#dataArray[6] += network.meanCoopDefDistance
-#dataArray[7] += network.meanDistInclusion
-#dataArray[8] += network.meanCoopFreq
