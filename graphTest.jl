6ec47b37612565b49475354275ffa984847afec8
yelostar <121648790+yelostar@users.noreply.github.com>
2023-08-06 21:38:46 -0400

    Added tests for distances

--- graphTest.jl
+++ graphTest.jl
@@ -46,3 +46,41 @@ gplot(edgeGraph, nodelabel=1:popSize)
 meanConnComponents = 0
 meanConnComponents += size(connected_components(edgeGraph))[1]
 println(meanConnComponents)
+
+function get_absolute_time()
+    s = 0.0
+    for i=1:10000000
+        s += i
+    end
+    sleep(1)
+end
+
+@time get_absolute_time()
+
+popSize = 100
+popLocations = zeros(Float64, popSize)
+            for(i) in 1:popSize
+                popLocations[i] = i #sets popLocations to [1, 2, ... popSize]
+            end
+            edgeMatrix = zeros(Float64, popSize, popSize)
+            for(i) in 1:popSize
+                linkWeights = (((popLocations[i].-popLocations)).^(2)).^(-0.25) #inverse of the sqrt of distance between locations
+                links = zeros(Float64, Int(popSize/2))
+                sample!(popLocations, Weights(linkWeights), links, replace=false)
+                for(j) in links
+                    edgeMatrix[i, Int(j)] = 1    
+                end
+            end  
+
+    dists = abs.(popLocations.-popLocations[1]) #.^(2).^(0.5) 
+    println(dists)
+    for(i) in 1:popSize
+        if(dists[i] < 6)
+            dists[i] = 1 #all distances 5 or under are set to 1
+        else
+            dists[i] -= 5
+        end
+    end
+    dists = dists.^(-0.5)
+    
+println(dists)
