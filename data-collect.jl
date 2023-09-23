ea2b5d7385f98fe8d44bcc1a4960cdf36a479e82
yelostar <121648790+yelostar@users.noreply.github.com>
2023-09-22 23:41:44 -0400

    Iterates by ben/cl after adding death-birth order

--- data-collect.jl
+++ data-collect.jl
@@ -13,12 +13,12 @@ addprocs(40)
     include("ONS_Fixed_Links.jl")
     while true
         pard = take!(inputs)
-        println(pard["cl"], " ", pard["distFactor"], " in pard")
-        coopFreq = runSimsReturn(; B=2.0, C=0.5, D=0.0, CL=pard["cl"], gen=100000, distInherit=true, distFactor=pard["distFactor"], findMom=neighborMom, pn=0.5, pnd=true, pr=0.0001, prd=false, muP=0.01, delta=0.5, sigmapn=0.01, sigmapr=0.01, reps=10)
+        println(pard["ben"], " ", pard["cl"], " in pard")
+        coopFreq = runSimsReturn(; B=pard["ben"], C=0.5, D=0.0, CL=pard["cl"], gen=100000, dbOrder=birthdeath, dbProb=0.0, pn=0.5, distInherit=false, pnd=true, pr=0.0001, prd=false, muP=0.01, delta=0.5, sigmapn=0.01, sigmapr=0.01, reps=10)
         Keys = ["pnc_end","pnd_end","prc_end","prd_end","degree","assortment","distance","inclusion","coopFreq","fitness","shortestPath","connComponents","meanConnCompSize","largestConnComp","meanConnDistance"]
         temp = Dict(zip(Keys, coopFreq))
         temp = merge(pard, temp)
-        println(temp["cl"], " ", temp["distFactor"], " CF: ", round(temp["coopFreq"]; digits = 3), " PNC: ", round(temp["pnc_end"]; digits = 3), " PND: ", round(temp["pnd_end"]; digits = 3), " PRC: ", round(temp["prc_end"]; digits = 3), " PRD: ", round(temp["prd_end"]; digits = 3))
+        println(temp["ben"], " ", temp["cl"], " CF: ", round(temp["coopFreq"]; digits = 3), " PNC: ", round(temp["pnc_end"]; digits = 3), " PND: ", round(temp["pnd_end"]; digits = 3), " PRC: ", round(temp["prc_end"]; digits = 3), " PRD: ", round(temp["prd_end"]; digits = 3) )
         put!(results, temp)
     end
 end
@@ -26,11 +26,11 @@ end
 function fill_inputs(range,pars, nruns)
     for(i) in 1:range #PNC/PND 
         for(j) in 1:range #PNR 
-            vals = (0.4*i/range, (0.5+0.5*j/range)) 
+            vals = (10*i/range, 0.4*j/range)
             temp = copy(pars)
-            temp["cl"] = vals[1]
-            temp["distFactor"] = vals[2]
-            println(temp["cl"], " ", temp["distFactor"], " into inputs")
+            temp["ben"] = vals[1]
+            temp["cl"] = vals[2]
+            println(temp["ben"], " ", temp["cl"], "into inputs")
             nruns+=1
             put!(inputs, temp)
             #vals_arr.push(vals)
@@ -47,8 +47,8 @@ end
             
     vals_arr = Array{Tuple}
     pars = Dict([
+            "ben" => 0.0,
             "cl" => 0.0,
-            "distFactor" => 0.0,
             "pnc_end" => 0.0,
             "pnd_end" => 0.0,
             "prc_end" => 0.0,
@@ -72,9 +72,9 @@ end
     end
 
 
-    file = "nmom_dist_small_range_pnd.csv"
+    file = "birth_death_pnd.csv"
         cols = push!(sort(collect(keys(pars))),
-                    ["cl", "distFactor"]...)
+                    ["ben", "cl"]...)
         dat = DataFrame(Dict([(c, Any[]) for c in cols]))
 
     for sim in 1:nruns
