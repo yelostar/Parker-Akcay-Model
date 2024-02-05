4b071c26b07dc1e8a1df0845a04d487616409627
yelostar <121648790+yelostar@users.noreply.github.com>
2024-02-05 09:04:22 -0500

    Update data-collect for 0.775-1.0 distFactor iteration

--- data-collect.jl
+++ data-collect.jl
@@ -1,3 +1,4 @@
+
 using JLD2
 using StatsBase
 using FileIO
@@ -6,32 +7,32 @@ using DataFrames
 using CSV
 using Distributed
 
-addprocs(40) 
+addprocs(40)
 @everywhere include("ONS_Fixed_Links.jl")
 
 @everywhere function run_worker(inputs, results)
     include("ONS_Fixed_Links.jl")
     while true
         pard = take!(inputs)
-        println(pard["cl"], " ", pard["distNeighborRange"], " in pard")
-        coopFreq = runSimsReturn(; B=2.0, C=0.5, D=0.0, CL=pard["cl"], gen=100000, distInherit=false, distFactor=1.0, findMom=neighborMom, neighborRange=Int(pard["distNeighborRange"]), pn=0.5, pnd=false, pr=0.0001, prd=false, muP=0.01, delta=0.5, sigmapn=0.01, sigmapr=0.01, reps=50)
-        Keys = ["pnc_end","pnd_end","prc_end","prd_end","degree","assortment","distance","inclusion","coopFreq","fitness","shortestPath","connComponents","meanConnCompSize","largestConnComp","meanConnDistance"]
+        println(pard["cl"], " ", pard["distFactor"], " in pard")
+        coopFreq = runSimsReturn(; B=2.0, C=0.5, D=0.0, CL=pard["cl"], gen=100000, dbOrder=deathbirth, distInherit=true, distFactor=pard["distFactor"], findMom=neighborMom, pn=0.5, pnd=false, pr=0.0001, prd=false, muP=0.01, delta=0.5, sigmapn=0.01, sigmapr=0.01, reps=50)
+        Keys = ["pnc_end", "pnd_end", "prc_end", "prd_end", "degree", "assortment", "distance", "inclusion", "coopFreq", "fitness", "shortestPath", "connComponents", "meanConnCompSize", "largestConnComp", "meanConnDistance"]
         temp = Dict(zip(Keys, coopFreq))
         temp = merge(pard, temp)
-        println(temp["cl"], " ", temp["distNeighborRange"], " CF: ", round(temp["coopFreq"]; digits = 3), " PNC: ", round(temp["pnc_end"]; digits = 3), " PND: ", round(temp["pnd_end"]; digits = 3), " PRC: ", round(temp["prc_end"]; digits = 3), " PRD: ", round(temp["prd_end"]; digits = 3))
+        println(temp["cl"], " ", temp["distFactor"], " CF: ", round(temp["coopFreq"]; digits=3), " PNC: ", round(temp["pnc_end"]; digits=3), " PND: ", round(temp["pnd_end"]; digits=3), " PRC: ", round(temp["prc_end"]; digits=3), " PRD: ", round(temp["prd_end"]; digits=3))
         put!(results, temp)
     end
 end
 
-function fill_inputs(range,pars, nruns)
-    for(i) in 1:range #x axis 
-        for(j) in 1:range #y axis
-            vals = (0.4*i/range, floor(-1+51*j/range)) 
-            temp = copy(pars) 
+function fill_inputs(range, pars, nruns)
+    for (i) in 1:range #PNC/PND 
+        for (j) in 1:range #PNR 
+            vals = (0.4 * i / range, (0.75 + 0.2 * j / range))
+            temp = copy(pars)
             temp["cl"] = vals[1]
-            temp["distNeighborRange"] = vals[2]
-            println(temp["cl"], " ", temp["distNeighborRange"], " into inputs")
-            nruns+=1
+            temp["distFactor"] = vals[2]
+            println(temp["cl"], " ", temp["distFactor"], " into inputs")
+            nruns += 1
             put!(inputs, temp)
             #vals_arr.push(vals)
         end
@@ -41,41 +42,41 @@ end
 
 #notebook for running below
 @time begin
-    range = 20        
-    inputs  = RemoteChannel(()->Channel{Dict}(range*range)) #2*nsets*maximum(pars["num_crossings"])
-    results = RemoteChannel(()->Channel{Dict}(range*range))
-            
+    range = 20
+    inputs = RemoteChannel(() -> Channel{Dict}(range * range)) #2*nsets*maximum(pars["num_crossings"])
+    results = RemoteChannel(() -> Channel{Dict}(range * range))
+
     vals_arr = Array{Tuple}
     pars = Dict([
-            "cl" => 0.0,
-            "distNeighborRange" => 0.0,
-            "pnc_end" => 0.0,
-            "pnd_end" => 0.0,
-            "prc_end" => 0.0,
-            "prd_end" => 0.0,
-            "degree" => 0.0,
-            "assortment" => 0.0,
-            "distance" => 0.0,
-            "inclusion" => 0.0,
-            "coopFreq" => 0.0,
-            "fitness" => 0.0,
-            "shortestPath" => 0.0,
-            "connComponents" => 0.0,
-            "meanConnCompSize" => 0.0,
-            "largestConnComp" => 0.0,
-            "meanConnDistance" => 0.0,
-        ])
-    nruns = fill_inputs(range,pars, 0)
+        "cl" => 0.0,
+        "distFactor" => 0.0,
+        "pnc_end" => 0.0,
+        "pnd_end" => 0.0,
+        "prc_end" => 0.0,
+        "prd_end" => 0.0,
+        "degree" => 0.0,
+        "assortment" => 0.0,
+        "distance" => 0.0,
+        "inclusion" => 0.0,
+        "coopFreq" => 0.0,
+        "fitness" => 0.0,
+        "shortestPath" => 0.0,
+        "connComponents" => 0.0,
+        "meanConnCompSize" => 0.0,
+        "largestConnComp" => 0.0,
+        "meanConnDistance" => 0.0,
+    ])
+    nruns = fill_inputs(range, pars, 0)
 
     for w in workers() # start tasks on the workers to process requests in parallel
         remote_do(run_worker, w, inputs, results)
     end
 
 
-    file = "neighborRange-1-50-no-pnd-more-reps.csv"
-        cols = push!(sort(collect(keys(pars))),
-                    ["cl", "distNeighborRange"]...)
-        dat = DataFrame(Dict([(c, Any[]) for c in cols]))
+    file = "distInherit_nmon_no_pnd.csv"
+    cols = push!(sort(collect(keys(pars))),
+        ["cl", "distFactor"]...)
+    dat = DataFrame(Dict([(c, Any[]) for c in cols]))
 
     for sim in 1:nruns
         # get results from parallel jobs
